7295399ee9ce2055928a7af8df93c08085dae494
Redrock912 <leset912@gmail.com>
2018-05-16 15:10:24 +0900

    Propeller Rotate + InputCommand initial commit

--- Source/P38CPP2/P38Pawn.cpp
+++ Source/P38CPP2/P38Pawn.cpp
@@ -15,14 +15,29 @@ AP38Pawn::AP38Pawn()
 
 	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
 	Box->SetupAttachment(RootComponent);
-
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
 	Body->SetupAttachment(Box);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>SM_Body(TEXT("StaticMesh'/Game/Data/SM_P38_Body.SM_P38_Body'"));
+	if (SM_Body.Succeeded())
+	{
+		Body->SetStaticMesh(SM_Body.Object);
+	}
 
 	Left = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left"));
 	Left->SetupAttachment(Body);
 	Right = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right"));
 	Right->SetupAttachment(Body);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>SM_Propeller(TEXT("StaticMesh'/Game/Data/SM_P38_Propeller.SM_P38_Propeller'"));
+	if (SM_Propeller.Succeeded())
+	{
+		Left->SetStaticMesh(SM_Propeller.Object);
+		Right->SetStaticMesh(SM_Propeller.Object);
+	}
+
+	Left->SetRelativeLocation(FVector(37, -21, 0));
+	Right->SetRelativeLocation(FVector(37, 21, 0));
 
 	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
 
@@ -39,7 +54,9 @@ void AP38Pawn::BeginPlay()
 void AP38Pawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
+	Left->AddLocalRotation(FRotator(0, 0, 1080 * DeltaTime));
+	Right->AddLocalRotation(FRotator(0, 0, 1080 * DeltaTime));
 }
 
 // Called to bind functionality to input
